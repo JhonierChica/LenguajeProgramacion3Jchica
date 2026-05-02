66ee61e8706c41eed236ed26101db22f86285c65
JhonierChica <jhonierandres125@gmail.com>
2026-05-02 10:49:30 -0500

    Initial commit from local project

---
+++ src/main/java/com/edu/uniremintong/jchica/lenguajesP3/LenguajesP3Application.java
@@ -0,0 +1,13 @@
+package com.edu.uniremintong.jchica.lenguajesP3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LenguajesP3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LenguajesP3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/edu/uniremintong/jchica/lenguajesP3/LenguajesP3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.uniremintong.jchica.lenguajesP3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LenguajesP3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
